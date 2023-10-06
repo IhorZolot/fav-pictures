a0e1fdcb817f885d18c90c4e8103cf5345c16fd9
Ihor <fedonn@ukr.net>
2023-10-06 11:24:25 +0300

    fix

--- src/components/ImageCounter/ImageCounter.jsx
+++ src/components/ImageCounter/ImageCounter.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 
 export const ImageCounter = ({ setCountToLoad }) => {
 	const [value, setValue] = useState('')
 
 	const handleSetCounte = () => {
-		if (value === '13') {
-			alert('error')
+		if (value <= 0 && value > '12') {
+			toast.error('Enter a value from 1 to 12', {
+				position: 'top-right',
+				autoClose: 2000,
+				hideProgressBar: false,
+			})
 		}
 		setCountToLoad(value)
 	}
